a774e5a5af1d8b2e5eef67a876dbeed11777dae7
Alvaro Castillo <alvaro.castillo777@gmail.com>
2026-06-08 17:31:04 +0200

    fix: adding condition for different flagging system

--- apps/web/app/lib/flags.ts
+++ apps/web/app/lib/flags.ts
@@ -1,3 +1,8 @@
 import { createClient } from "@vercel/flags-core";
 
-export const flagsClient = createClient(process.env.DEV_FLAGS_CLIENT_KEY);
+const clientCreds =
+  process.env.NODE_ENV === "production"
+    ? process.env.PROD_FLAGS_CLIENT_KEY
+    : process.env.DEV_FLAGS_CLIENT_KEY;
+
+export const flagsClient = createClient(clientCreds);
